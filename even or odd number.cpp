9bcf58ae2daf4aabcf8f9dcb1553d3324c85e781
Dev Kumar <110048121+DevKumar57-67@users.noreply.github.com>
2024-05-25 01:15:50 +0530

    Add files via upload

---
+++ even or odd number.cpp
@@ -0,0 +1,14 @@
+
+// program to check whether the number is odd or even
+#include <iostream>
+using namespace std;
+int main(){
+  int x;
+  cout<<"Enter the number to check";
+  cin>>x;
+  if(x%2==0)
+  cout<<"The number is even";
+  else
+  cout<<"The number is odd";
+  return 0;
+}
