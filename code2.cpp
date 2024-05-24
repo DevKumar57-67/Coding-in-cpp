3dd84c40f28838d04692131821f837b3f74e8de8
Dev Kumar <110048121+DevKumar57-67@users.noreply.github.com>
2024-05-25 01:14:50 +0530

    Add files via upload

---
+++ code2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+    
+    int a,b,sum,sub,mul,div;
+    a=56;
+    b=89;
+    sum=a+b;
+    sub=a-b;
+    mul=a*b;
+    div=a/b;
+    cout <<"sum is : \n"<< sum;
+    cout <<"sub is : \n"<< sub;
+    cout <<"mul is : \n"<< mul;
+    cout <<"div is : \n"<< div;
+}
